7ec643ad9f5c41c2c91fdf7fdbd13e2e234429dc
wenle.bwl <wenle.bwl@alibaba-inc.com>
2017-03-10 23:18:02 +0800

    add solution for "find mode in bst"

---
+++ java/FindModeInBST.java
@@ -0,0 +1,23 @@
+/**
+ * date 17/3/9
+ *
+ * @author wenle
+ * @see https://leetcode.com/problems/find-mode-in-binary-search-tree/?tab=Description
+ */
+public class FindModeInBST {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public int[] findMode(TreeNode root) {
+        return null;
+    }
+
+}
