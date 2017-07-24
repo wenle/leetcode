7291f3ee287f165cd803ce39beb0894a9781a3a8
wenle.bwl <wenle.bwl@alibaba-inc.com>
2017-07-24 10:43:48 +0800

    add solutions

---
+++ src/main/java/jianzhioffer/Question13.java
@@ -0,0 +1,54 @@
+package jianzhioffer;
+
+/**
+ * date 17/7/24
+ *
+ * @author wenle
+ */
+public class Question13 {
+
+    static class Node {
+        int value;
+        Node next;
+
+        public int getValue() {
+            return value;
+        }
+
+        public void setValue(int value) {
+            this.value = value;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+    }
+
+    public static Node deleteNodeO1(Node head, Node toBeDeleted) {
+
+        if(head == toBeDeleted) {
+            return null;
+        }
+
+        if(toBeDeleted.getNext() == null) {
+
+            Node p = head;
+            Node pre = null;
+            while(p.getNext() != null) {
+                pre = p;
+                p = p.getNext();
+            }
+            pre.setNext(null);
+            return head;
+        }
+
+        toBeDeleted.setValue(toBeDeleted.getNext().getValue());
+        toBeDeleted.setNext(null);
+        return head;
+    }
+
+}
